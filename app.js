5c0ff360eb90c094541077ea5c1a102e4e4bd813
davideaded <david.castrodc033@gmail.com>
2025-07-04 13:16:51 -0300

    signup page

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require('path');
+
+const serverAddress = process.env.ADDRESS || "127.0.0.1";
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/sign-up", (_req, res) => {
+    res.render("signup");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err);
+});
+app.listen(port, serverAddress, () => console.log(`Listening on ${serverAddress}:${port}`));
